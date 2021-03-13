e2b176bd583a91552c51c8a15bc61c13874275a3
Shashank <shashank.niranjan@gmail.com>
2021-03-13 13:12:49 +0530

    Added remove

--- src/com/dataStructures/lists/LinkedList.java
+++ src/com/dataStructures/lists/LinkedList.java
@@ -2,74 +2,123 @@ package com.dataStructures.lists;
 
 public class LinkedList {
 
-	Node head;
-	Node tail;
+    Node head;
+    Node tail;
 
-	int length;
+    int length;
 
-	class Node {
+    class Node {
 
-		int value;
-		Node next;
+        int value;
+        Node next;
 
-		Node(int value) {
-			this.value = value;
+        Node(int value) {
+            this.value = value;
 
-		}
+        }
 
-		@Override
-		public String toString() {
-			return "Node{" + "value=" + value + ", next=" + next + '}';
-		}
-	}
+        @Override
+        public String toString() {
+            return "Node{" + "value=" + value + ", next=" + next + '}';
+        }
+    }
 
-	LinkedList(int value) {
-		this.head = new Node(value);
+    LinkedList(int value) {
+        this.head = new Node(value);
 
-		this.tail = this.head;
-		this.length = 1;
-	}
+        this.tail = this.head;
+        this.length = 1;
+    }
 
-//	LinkedList insert( int index,int value)
-//	{
-//
-//
-//
-//	}
-//
+    LinkedList insert(int index, int value) {
 
-	LinkedList append(int value) {
 
-		this.tail.next = new Node(value);
 
-		this.tail = this.tail.next;
+        if(index>this.length)
+        {
+            append(value);
+        }
+        else {
+            Node node = new Node(value);
 
-		length++;
-		return this;
+            Node iterator = this.head;
 
-	}
+            for (int i = 0; i < index - 1; i++) {
 
-	LinkedList prepend(int value) {
-		Node newHead = new Node(value);
-		newHead.next = this.head;
-		this.head = newHead;
-		length++;
-		return this;
+                iterator = iterator.next;
 
-	}
 
-	@Override
-	public String toString() {
-		return "LinkedList{" + "head=" + head + ", tail=" + tail + ", size=" + length + '}';
-	}
+            }
 
-	public static void main(String[] args) {
-		LinkedList linkedList = new LinkedList(10);
-		System.out.println(linkedList);
-		System.out.println(linkedList.append(4));
-		System.out.println(linkedList.append(1));
-		System.out.println(linkedList.prepend(3));
-		System.out.println(linkedList.prepend(4));
-	}
+            Node temp = iterator;
+            node.next = iterator.next;
+            temp.next = node;
+            this.length++;
+
+        }
+        return this;
+
+
+    }
+
+
+    LinkedList remove(int index) {
+
+        if(index>this.length)
+        {
+            return this;
+        }
+
+
+        Node iterator = this.head;
+
+        for (int i = 0; i < index-1; i++) {
+
+            iterator = iterator.next;
+
+
+
+        }
+        // - - -
+        iterator.next =iterator.next.next;
+        return this;
+    }
+
+
+    LinkedList append(int value) {
+
+        this.tail.next = new Node(value);
+
+        this.tail = this.tail.next;
+
+        length++;
+        return this;
+
+    }
+
+    LinkedList prepend(int value) {
+        Node newHead = new Node(value);
+        newHead.next = this.head;
+        this.head = newHead;
+        length++;
+        return this;
+
+    }
+
+    @Override
+    public String toString() {
+        return "LinkedList{" + "head=" + head + ", tail=" + tail + ", size=" + length + '}';
+    }
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(1);
+        System.out.println(linkedList);
+        System.out.println(linkedList.append(3));
+        System.out.println(linkedList.append(4));
+//		System.out.println(linkedList.prepend(3));
+//		System.out.println(linkedList.prepend(4));
+        System.out.println(linkedList.insert(10, 2));
+        System.out.println(linkedList.remove(10));
+    }
 
 }
